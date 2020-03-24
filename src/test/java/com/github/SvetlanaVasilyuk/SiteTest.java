1f366cd4622e7206ca186875f04280d7a603e767
Svetlana Vasilyuk <vasilyuk@i-teco.ru>
2020-03-24 20:41:20 +0300

    Скорректировано название класса

--- src/test/java/com/github/SvetlanaVasilyuk/SiteTest.java
+++ src/test/java/com/github/SvetlanaVasilyuk/SiteTest.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class YandexTest {
+public class SiteTest {
 
     private WebDriver webDriver;
 
